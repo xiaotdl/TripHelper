ce1afdd0db96d4d5a3fd89d3d56ad49fe1802638
Xiaotian Li <xiaotdl@gmail.com>
2013-11-02 16:39:30 -0600

    captions of pics added

--- js/triphelper.js
+++ js/triphelper.js
@@ -1,9 +1,24 @@
-/*javascript goes here!*/
+/* API Keys */
+var pearsonApiKey = "?apiKey=AM3Uez7G9OyJrQZY1bCxXE28lhRHfW8Y";
+var pearsonBaseUrl = "http://api.pearson.com/v2/travel/";
 
 var locationString = "";
 
-$("citySearch").on('click')
+function showMore(){
+	$('#moreOptions').show("blind");
+}
 
 function CitySearch(cityName) {
 
 }
+
+function getTravelCategories() {
+	$.getJSON(pearsonBaseUrl+"categories"+pearsonApiKey,
+		function(data){
+			$.each( data.results, function( key, val ) {
+				if(val.name!="Attractions") //this is a bad category
+			    $('#categories').append("<label class=\"btn btn-primary\"><input type=\"checkbox\">"+val.name+"</label>");
+			  });
+		});
+}
+
---
+++ js/triphelper.js.bak
@@ -0,0 +1,9 @@
+/*javascript goes here!*/
+
+var locationString = "";
+
+$("citySearch").on('click')
+
+function CitySearch(cityName) {
+
+}
